eee2c709733707c97a729b47f710edd2ff48e4b5
Samantha Finley <srfinley@uchicago.edu>
2020-04-16 17:36:15 -0400

    997 moves to traverse the maze

--- adv.py
+++ adv.py
@@ -5,6 +5,20 @@ from world import World
 import random
 from ast import literal_eval
 
+class Queue():
+    # copied from util.py
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 # Load world
 world = World()
 
@@ -13,8 +27,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -38,6 +52,55 @@ def add_current_room_to_conns():
         next_room = getattr(player.current_room, f"{direction}_to")
         connections[player.current_room.id][direction] = next_room.id
 
+# def reverse_direction(direction):
+#     di = {'n': 's',
+#           's': 'n',
+#           'e': 'w',
+#           'w': 'e'}
+#     return di[direction]
+
+def get_adj_ids(id):
+    """Takes a room ID and returns the IDs of adjascent rooms"""
+    results = []
+    for direction in connections[id]:
+        results.append(connections[id][direction])
+    return results
+
+def bfs():
+    """Starting from the player's current position,
+    return the sequence of rooms that will take them to
+    the nearest unexplored room"""
+    qq = Queue()
+    qq.enqueue([player.current_room.id])
+    visited = set()
+    while qq.size() > 0:
+        path = qq.dequeue()
+        if path[-1] not in connections:
+            return path
+        if path[-1] not in visited:
+            visited.add(path[-1])
+            for next_room in get_adj_ids(path[-1]):
+                new_path = path + [next_room]
+                qq.enqueue(new_path)
+    return []
+
+def get_path():
+    """Based on the room sequence from bfs,
+    returns the series of cardinal directions to take
+    to follow the path"""
+    route = bfs()
+    path = []
+    for index, stop in enumerate(route[1:]):
+        # note: the enumerate object itself is zero-indexed
+        # previous stop id is route[index]
+        # append the route[index] -> stop direction
+        for direction in connections[route[index]]:
+            if connections[route[index]][direction] == stop:
+                path.append(direction)
+    return path[::-1]
+
+set_path = []
+
 # main travel loop
 add_current_room_to_conns()
 while len(room_graph) > len(connections):
@@ -48,10 +111,17 @@ while len(room_graph) > len(connections):
     # go through the first door that leads to a new room
     for option in options:
         if connections[player.current_room.id][option] not in connections:
+            set_path = []
             door = option
     # if all exits from a room have previously been explored
     if door is None:
-        door = random.choice(options)
+        # BFS for the next unexplored room
+        if len(set_path) == 0:
+            set_path = get_path()
+            # print(set_path)
+        door = set_path[-1]
+        set_path = set_path[:-1]
+        
 
     # go
     player.travel(door)
